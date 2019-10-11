c0c503f86ab66ab057916b29a37a265e7b14a4f1
Choudhary <sonu.choudhary@accenture.com>
2019-10-11 17:09:24 +0530

    updated the test class methods

--- src/TestClassed/TestClass.java
+++ src/TestClassed/TestClass.java
@@ -9,36 +9,43 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestClass {
 
 	WebDriver driver;
 	
-	
+	@BeforeMethod
+	public void initalizeDriver() {
+		String browser = "Chrome";
+		if(browser == "Chrome") {
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\sonu.choudhary\\Downloads\\chromedriver_win32\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}else {
+			driver = new FirefoxDriver();
+		}
+	}
 	@Test
 	public void Test1() {
-		
-
-	
-				String browser = "Chrome";
-				if(browser == "Chrome") {
-					System.setProperty("webdriver.chrome.driver", "C:\\Users\\sonu.choudhary\\Downloads\\chromedriver_win32\\chromedriver.exe");
-					driver = new ChromeDriver();
-				}else {
-					driver = new FirefoxDriver();
-				}
-			 
-				driver.get("https://www.seleniumhq.org");
-	            
+		        driver.navigate().to("https://www.seleniumhq.org");      
 				driver.findElement(By.id("q"));
+				System.out.println("title of the page is "+ driver.getTitle());
 				Assert.assertEquals(true, true);
 			}
 
 	@Test
 	public void Test2() {
+		driver.navigate().to("https://www.google.com");  
 		String title = "Selenium";
+		System.out.println("title of the page is "+ driver.getTitle());
 		Assert.assertEquals(true, true);
+	}
+	
+	@AfterMethod
+	public void Quit() {
+		driver.quit();
 	}
 
 	
